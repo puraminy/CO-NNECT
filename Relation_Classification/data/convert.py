19b6c20c88d126d0a07406f7cbadcc67c9dfa91c
Ahmad <pouramini@gmail.com>
2021-07-01 13:20:02 +0430

    a python script for converting Atomic to data required for classifer

--- Relation_Classification/data/convert.py
+++ Relation_Classification/data/convert.py
@@ -1,43 +1,102 @@
 import pandas as pd
 from tqdm import tqdm
 from pathlib import Path
+from sklearn.utils import shuffle
+import click
+dest = "/home/pouramini/CO-NNECT/Relation_Classification/data"
+valid_dir = "/drive3/pouramini/data/atomic/en_fa/sample_valid_1k"
+ulist = ["other", "xAttr", "xIntent", "xNeed", "xWant", "xReact", "xEffect", "oReact", "oEffect", "oWant"]
+@click.command()
+@click.argument("fname", type=str)
+@click.option(
+    "--path",
+    envvar="PWD",
+    #    multiple=True,
+    type=click.Path(),
+    help="The current path (it is set by system)"
+)
+@click.option(
+    "--size",
+    default=1000,
+    type=int,
+    help=""
+)
+@click.option(
+    "--out",
+    default="",
+    type=str,
+    help=""
+)
+@click.option(
+    "--tag",
+    default="",
+    type=str,
+    help=""
+)
+@click.option(
+    "--lang",
+    default="en",
+    type=str,
+    help=""
+)
+def main(path, fname, size, out, tag, lang):
+    if fname == "all":
+        for lang in ["en", "fa"]:
+            for t in ulist[1:]:
+                conv(path, "", tag=t, lang=lang, size=size, out=out)
+    else:
+        conv(path, fname, size, out, tag, lang)
 
-data_dir = "/drive2/pouramini/pet/data/atomic"
-def conv(tag):
-    train_fname = data_dir + f'/atomic_train_nn_5k_per_prefix_{tag}_other.tsv'
-    valid_fname = data_dir + f'/atomic_validation_nn_1k_per_prefix_{tag}_other.tsv'
-    train = pd.read_table(train_fname)
-    valid = pd.read_table(valid_fname)
+def conv(path, fname, size, out="", tag="", lang="en"):
+    other = ""
+    if fname:
+        tag_fname = path + "/" + fname
+    else:
+        tag_fname = path + f'/en_fa_{tag}.tsv'
+    other_fname = valid_dir + "/en_fa_" + tag + "_other.tsv"
+    tag_df = pd.read_table(tag_fname)
+    other_df = pd.read_table(other_fname)
+
+    tag_df = pd.concat([tag_df, other_df], ignore_index=True)
+
+    tag_df = tag_df.groupby("prefix").sample(n=size, random_state=1)
+    tag_df = tag_df.sample(frac=1, random_state=1)
+    tn = "k".join(str(size).rsplit("000", 1))
+    tn = tn.replace("kk", "m") 
     #valid = valid.dropna()
-    #train = train.dropna()
-    print(len(train))
-    print(len(valid))
-    labels, uniques = train.prefix.factorize()
-    ulist = uniques.tolist()
+    #tag_df = tag_df.dropna()
+    print(len(tag_df))
+    #labels, uniques = tag_df.prefix.factorize()
+    #ulist = uniques.tolist()
     print("labels:", ulist)
-    with open(f'labels_{tag}.txt','w') as f:
+    with open(f'{dest}/labels.txt','w') as f:
         for label in ulist:
             print(label.strip(),' ', ulist.index(label), file=f)
     def convert(df, inp):
         with open(f'{inp}.src', 'w') as src,  open(f'{inp}.trg', 'w') as trg:
             _max = 0
             for idx, row in tqdm(df.iterrows(), total=len(df)):
-                s = f'{row["input_text"]} <mask> {row["target_text"]}'
+                if lang == "en":
+                    s = f'{row["input_text"]} <mask> {row["target_text"]}'
+                elif lang == "fa":
+                    s = f'{row["input_text_fa"]} <mask> {row["target_text_fa"]}'
                 if len(s) > _max:
                     _max = len(s)
                 print(s, file= src)
                 l = ulist.index(row["prefix"])
-                assert l in range(9), "Uknown lable"
+                assert l in range(10), "Uknown lable"
                 print(l, file= trg)
 
             print("max length:", _max)
-
-    convert(train, Path(train_fname).stem)
-    convert(valid, Path(valid_fname).stem)
+    if not out:
+        out = lang + "_" + tag + "_" + tn
+    else:
+        out = lang + "_" + tag + "_" + tn + "_" + out
+    out = dest + "/" + out
+    print(out, ":", len(tag_df))
+    convert(tag_df, out) 
     print("finished")
 
+if __name__ == "__main__":
+    main()
 
-l = ["xIntent", "xNeed", "xWant", "xReact", "xEffect", "oReact", "oEffect", "oWant"]
-
-for t in l:
-    conv(t)
